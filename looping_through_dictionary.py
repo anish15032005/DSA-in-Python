44868cb990fe6647aee2e451709c3ec33f397f0f
Anish Mansuri <146653155+mike-wazoski-1991@users.noreply.github.com>
2024-08-22 13:09:55 +0530

    Code uploaded on Github.

---
+++ looping_through_dictionary.py
@@ -0,0 +1,11 @@
+places = {
+    "New York": "Statue of Liberty",
+    "New Delhi": "Red Fort",
+    "Hyderabad": "Charminaar",
+    "Agra": "Taj Mahal",
+    "Kolkata": "Victoria Memorial"
+}
+
+#It will only print keys
+for i in places:
+    print(i)
