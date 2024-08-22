fc87a44ba1578ba20bafd277f4955daacb910376
Anish Mansuri <146653155+mike-wazoski-1991@users.noreply.github.com>
2024-08-22 22:45:10 +0530

    Code uploaded on Github.

---
+++ key_with_multiple_values.py
@@ -0,0 +1,9 @@
+travel_log = {
+    "Bhopal" : ["LNCT","Shreyansh","Poha Jalebi"],
+    "Indore" : ["IIT Indore","Kanwaldeep","Dahi Bada"],
+    "Kolkata" : ["IIEST Shibpur","Bilal","Keema Paratha"]
+}
+
+print(travel_log)
+
+print(travel_log["Kolkata"][2])
